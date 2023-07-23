c7424ec2e9f411c21b65cec70ee575c4d5231b8c
Netology <orjuice@mail.ru>
2023-07-23 22:20:02 +0300

    Test failed

---
+++ src/test/java/FormDeliveryTest.java
@@ -0,0 +1,23 @@
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FormDeliveryTest {
+    @Test
+    void shouldTestFormDelivery() {
+        open("http://0.0.0.0:9999");
+
+//SelenideElement form = $ ("[data-test-id = form]");
+         $("[data-test-id = name] input").setValue("Julia Максимова");
+         $("[data-test-id = phone] input").setValue("+79166060000");
+      $("[data-test-id = agreement]").click();
+     $(".button").click();
+     $ ("[data-test-id = order-success").shouldHave(exactText("Ваша заявка успешно отправлена! " + "Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+}
+
